0baec9f540dc7e8a5c558485fa683972c04657b3
Adrián Cantón Fernández <adrcanfer@alum.us.es>
2019-03-12 21:44:15 +0100

    Added contact form without css

--- capsulefyweb/main/views.py
+++ capsulefyweb/main/views.py
@@ -1,4 +1,6 @@
 from django.shortcuts import render
+from main.forms import ContactForm
+import smtplib
 
 
 def index(request):
@@ -14,6 +16,25 @@ def about(request):
 
 
 def contact(request):
-    return render(request, 'contact.html')
+    enterpriseEmail = "capsulefy.communications@gmail.com"
+    if request.method == 'POST':
+        form = ContactForm(request.POST)
+        if form.is_valid():
+
+            name = form.cleaned_data["Name"]
+            email = form.cleaned_data["Email"]
+            message = form.cleaned_data["Message"]
+            form = ContactForm()
+            server = smtplib.SMTP_SSL('smtp.gmail.com', 465)
+            server.login("capsulefy.communications@gmail.com", "aG7nOp4FhG")
+            msg = name+"\n" + email + "\n" + message
+            msg = msg.encode('utf-8')
+            server.sendmail(msg=msg, from_addr=email, to_addrs=[enterpriseEmail])
+            return render(request, 'contact.html', {'form': form})
+    else:
+        form = ContactForm()
+
+    return render(request, 'contact.html', {'form': form})
+
 
 

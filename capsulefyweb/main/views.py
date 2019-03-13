e804f7a700e7644b7510bdee4b0a40a632f61eba
Adrián Cantón Fernández <adrcanfer@alum.us.es>
2019-03-13 18:05:32 +0100

    Unified page

--- capsulefyweb/main/views.py
+++ capsulefyweb/main/views.py
@@ -4,7 +4,23 @@ import smtplib
 
 
 def index(request):
-    return render(request, 'index.html')
+    enterpriseEmail = "capsulefy.communications@gmail.com"
+    if request.method == 'POST':
+        form = ContactForm(request.POST)
+        if form.is_valid():
+            name = form.cleaned_data["Name"]
+            email = form.cleaned_data["Email"]
+            message = form.cleaned_data["Message"]
+            form = ContactForm()
+            server = smtplib.SMTP_SSL('smtp.gmail.com', 465)
+            server.login(enterpriseEmail, "aG7nOp4FhG")
+            msg = name + "\n" + email + "\n" + message
+            msg = msg.encode('utf-8')
+            server.sendmail(msg=msg, from_addr=email, to_addrs=[enterpriseEmail])
+            return render(request, 'index.html', {'form': form})
+    else:
+        form = ContactForm()
+    return render(request, 'index.html', {'form': form})
 
 
 def team(request):

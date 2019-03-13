6b3bc2256b19af1466ba0fb8371d61882b40027b
Adrián Cantón Fernández <adrcanfer@alum.us.es>
2019-03-13 18:09:20 +0100

    Remove unnecessary files

--- capsulefyweb/main/views.py
+++ capsulefyweb/main/views.py
@@ -21,36 +21,3 @@ def index(request):
     else:
         form = ContactForm()
     return render(request, 'index.html', {'form': form})
-
-
-def team(request):
-    return render(request, 'team.html')
-
-
-def about(request):
-    return render(request, 'about.html')
-
-
-def contact(request):
-    enterpriseEmail = "capsulefy.communications@gmail.com"
-    if request.method == 'POST':
-        form = ContactForm(request.POST)
-        if form.is_valid():
-
-            name = form.cleaned_data["Name"]
-            email = form.cleaned_data["Email"]
-            message = form.cleaned_data["Message"]
-            form = ContactForm()
-            server = smtplib.SMTP_SSL('smtp.gmail.com', 465)
-            server.login(enterpriseEmail, "aG7nOp4FhG")
-            msg = name+"\n" + email + "\n" + message
-            msg = msg.encode('utf-8')
-            server.sendmail(msg=msg, from_addr=email, to_addrs=[enterpriseEmail])
-            return render(request, 'contact.html', {'form': form})
-    else:
-        form = ContactForm()
-
-    return render(request, 'contact.html', {'form': form})
-
-
-

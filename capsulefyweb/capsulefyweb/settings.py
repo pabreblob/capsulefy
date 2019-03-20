a95144bc779f7823f494c7cef3052eab7c642afb
juanrr950 <juanrr950@gmail.com>
2019-03-20 15:17:13 +0100

    Django Model
    Close #59

--- capsulefyweb/capsulefyweb/settings.py
+++ capsulefyweb/capsulefyweb/settings.py
@@ -74,15 +74,25 @@ WSGI_APPLICATION = 'capsulefyweb.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/2.1/ref/settings/#databases
-
+'''
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
-
-
+'''
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'capsulefybd',
+        'USER': 'capsulefyu',
+        'PASSWORD':'capsulefy',
+        'HOST':'localhost',
+        'PORT':'',
+        
+    }
+}
 # Password validation
 # https://docs.djangoproject.com/en/2.1/ref/settings/#auth-password-validators
 

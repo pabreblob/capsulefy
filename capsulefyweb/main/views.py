8106430553fe771012dc2ae3675751e29eab0e78
Daniel <danielcarpioc@gmail.com>
2019-03-24 18:12:12 +0100

    Selección de compra

--- capsulefyweb/main/views.py
+++ capsulefyweb/main/views.py
@@ -218,3 +218,6 @@ def deleteCapsule(request, pk):
     capsule.delete()
     return HttpResponseRedirect('/')
 
+@login_required
+def select_capsule(request):
+    return render(request, 'capsule/select_capsule.html')

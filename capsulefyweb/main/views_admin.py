69b038b9cc7b5f1677c8a86a3ceb6961986baeb0
juanrr950 <juanrr950@gmail.com>
2019-04-09 18:19:24 +0200

    Dashboard
    Close #118

--- capsulefyweb/main/views_admin.py
+++ capsulefyweb/main/views_admin.py
@@ -2,6 +2,8 @@ from .models import User
 from django.shortcuts import render, redirect, Http404, HttpResponse
 from django.contrib.auth.decorators import login_required
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
+from main.models import Capsule, Module
+from datetime import timezone, datetime
 
 @login_required
 def list(request):
@@ -35,3 +37,33 @@ def ajax_ban(request):
         return HttpResponse("True")
     except:
         return HttpResponse("False")
+    
+@login_required
+def dashboard(request):
+    if not request.user.is_superuser:
+        raise Http404("")
+    '''Number of registered user, number of banned users, 
+    number of premium capsules, number of free capsules, 
+    ratio of premium capsules/free capsules, 
+    number of published capsules, number of unpublished capsules, 
+    ratio published/unpublished
+    '''
+    data={}
+    data['registeredUser']=User.objects.all().count()
+    data['bannedUser']=User.objects.filter(is_active=False).count()
+    data['premiumCapsules']=Capsule.objects.filter(capsule_type='M').count()
+    data['freeCapsules']=Capsule.objects.filter(capsule_type='F').count()
+    data['totalCapsules']=Capsule.objects.all().count()
+    data['totalModules']=Module.objects.all().count()
+    data['publishedModules']=Module.objects.filter(release_date__lte=datetime.now(timezone.utc)).count()
+    data['unpublishedModules']=Module.objects.filter(release_date__gt=datetime.now(timezone.utc)).count()
+    data['ratioPremiumFree']=data['premiumCapsules']/data['freeCapsules']
+    data['ratioPubliUnpubli']=data['publishedModules']/data['unpublishedModules']
+    data['porModPubli']=round(data['publishedModules']/data['totalModules']*100,1)
+    data['porModunPubli']=round(data['unpublishedModules']/data['totalModules']*100,1)
+    data['porCapFree']=round(data['freeCapsules']/data['totalCapsules']*100,1)
+    data['porCapPre']=round(data['premiumCapsules']/data['totalCapsules']*100,1)
+    
+    return render(request, 'admin/dashboard.html', {"data": data})
+    
+    

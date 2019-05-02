67121b98d33d8ba8c1aa8b4f8c8e0708dc0e2e62
pabreblob <pabreblob@alum.us.es>
2019-05-02 17:06:15 +0200

    fixed password error message in sign in

--- capsulefyweb/main/validators.py
+++ capsulefyweb/main/validators.py
@@ -4,6 +4,5 @@ import re
 def validatePassword(password):
     if not re.match(r'^(?=.*[a-z])(?=.*?[A-Z])(?=.*\d)[A-Za-z\d@$!¡¿?/%*#?&.:,;Çç\-*+\\<>]{8,}$', password):
         
-        raise ValidationError('Password incorrect, at least 8 characters, one number, one capital letter, one small letter '+\
-                                                       ' (Optinal allow: @$!¡¿?/%*#?&.:,;Çç-*+\<>).'
+        raise ValidationError('Password incorrect, at least 8 characters, one number, one capital letter, one small letter.'
         )
